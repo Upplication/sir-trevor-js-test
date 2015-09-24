e7e47c16e95d8d519cfd58cd98ce7a11bcdffaa4
Souler <juanjo.herrero@upplication.com>
2015-09-24 14:17:04 +0200

    Added a mock function for seeing it in action!

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-require("./slidesshare");
-require("./image-extended");
+// require("./slidesshare");
+// require("./image-extended");
+require("./st-adapter");
 require("./button");
 require("./widget");
 
@@ -16,8 +17,71 @@ jQuery(function(){
   $('form').submit(function(event){
       // get the data!
       console.log("$(form).submit()");
-      console.log($('form').serialize());
-      console.log(SirTrevor.getInstance().store.retrieve().data);
+      // console.log($('form').serialize());
+      // console.log(SirTrevor.getInstance().store.retrieve().data);
+      // console.log(JSON.stringify(SirTrevor.getInstance().store.retrieve().data));
       event.preventDefault();
   });
 });
+
+window.mock = function() {
+  // Proc the data save
+  $('form#st-form').submit();
+
+  var st = SirTrevor.getInstance();
+  var sta = new SirTrevorAdapter();
+  var args = [];
+
+  var clean = function(a) {
+    return a.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  }
+
+  var getStData = function() {
+    console.log('getStData');
+    var d1 = st.store.retrieve().data;
+    var d = JSON.stringify(d1, null, 2);
+    d = clean(d);
+    $('#st-json-raw').html(d);
+    args.push(d1);
+  }
+
+  var generateHTML = function() {
+    console.log('generateHTML');
+    var data = args.pop();
+    var html = sta.toHTML(data)
+    $('#st-html').html(clean(html));
+    $('#app-html').html(html);
+    args.push(html);
+  }
+
+  var resetSirTrevor = function() {
+    console.log('resetSirTrevor');
+    st.$el.val('');
+    st.reinitialize();
+  }
+
+  var getJSONfromHTML = function() {
+    console.log('getJSONfromHTML');
+    var html = args.pop();
+    var d1 = sta.fromHTML(html);
+    var d = JSON.stringify(d1, null, 2);
+    d = clean(d);
+    $('#st-json-parsed').html(d);
+    args.push(d1);
+  }
+
+  var loadIntoSirTrevor = function() {
+    console.log('loadIntoSirTrevor');
+    var data = args.pop();
+    var d = { data: data };
+    st.$el.val(JSON.stringify(d));
+    st.reinitialize();
+  }
+
+  setTimeout(getStData, 0 * 1000);
+  setTimeout(generateHTML, 1 * 1000);
+  setTimeout(resetSirTrevor, 2 * 1000);
+  setTimeout(getJSONfromHTML, 3 * 1000);
+  setTimeout(loadIntoSirTrevor, 4 * 1000);
+
+}
